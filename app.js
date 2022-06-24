78ab1d04dc11eea0529d803d6feb5bbf30f2ee3f
jebarsonponraj <51267066+jebarsonponraj@users.noreply.github.com>
2022-06-24 11:50:27 +0530

    Refactor some code

--- app.js
+++ app.js
@@ -1,38 +1,40 @@
 
 
-_u = _.capitalize();
+
+const renderHtml = function(data){
+    document.getElementById("active").textContent = data.active.toLocaleString();
+    document.getElementById("cases").textContent = data.cases.toLocaleString();
+    document.getElementById("death").textContent = data.deaths.toLocaleString();
+    document.getElementById("recovered").textContent = data.recovered.toLocaleString();
+}
+
 fetch('https://disease.sh/v3/covid-19/all')
   .then((response) => {
     return response.json();
   })
   .then((data) => {
     console.log(data);
-    document.getElementById("active").innerHTML = data.active.toLocaleString();
-    document.getElementById("cases").innerHTML = data.cases.toLocaleString();
-    document.getElementById("death").innerHTML = data.deaths.toLocaleString();
-    document.getElementById("recovered").innerHTML = data.recovered.toLocaleString();
+    renderHtml(data);
+    
   });
 
 var button = document.querySelector("button");
 button.addEventListener('click', () => {
-  var country = _.capitalize(document.getElementById('searchButton').value);
+  var country = document.getElementById('searchButton').value;
   console.log(country);
-  fetch('https://corona.lmao.ninja/v2/countries/' + country + '?yesterday=true&strict=true&query')
+  fetch(`https://corona.lmao.ninja/v2/countries/${country}?yesterday=true&strict=true&query`)
     .then((response) => {
       return response.json();
     })
     .then((data) => {
       console.log(data);
-      if (country == data.country) {
-
-        document.getElementById("places").innerHTML = data.country;
-        document.getElementById("active").innerHTML = data.active.toLocaleString();
-        document.getElementById("cases").innerHTML = data.cases.toLocaleString();
-        document.getElementById("death").innerHTML = data.deaths.toLocaleString();
-        document.getElementById("recovered").innerHTML = data.recovered.toLocaleString();
-      } else {
-
-        document.getElementById("places").innerHTML = "Oops! No country Found";
+      if (!data.country) {
+          
+        document.getElementById("places").innerHTML = data.message;
+    } else {
+        document.getElementById("places").textContent = data.country;
+        renderHtml(data);
+        
       }
 
     });
