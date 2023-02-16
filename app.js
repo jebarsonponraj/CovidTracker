5216acc36980bdfc7dd895d310e969768ba939fb
jebarsonponraj <51267066+jebarsonponraj@users.noreply.github.com>
2023-02-16 19:21:50 +0530

    Fix API Issue

--- app.js
+++ app.js
@@ -1,41 +1,56 @@
 
 
 
+
 const renderHtml = function(data){
-    document.getElementById("active").textContent = data.active.toLocaleString();
-    document.getElementById("cases").textContent = data.cases.toLocaleString();
-    document.getElementById("death").textContent = data.deaths.toLocaleString();
-    document.getElementById("recovered").textContent = data.recovered.toLocaleString();
+
+    if(data["Active Cases_text"] == "" ){
+    document.getElementById("active").textContent = "Nill";
+    }
+    else{
+        const activeCases = data["Active Cases_text"].toLocaleString();
+        document.getElementById("active").textContent = activeCases.slice(1,activeCases.length)
+    }
+    document.getElementById("cases").textContent = data["Total Cases_text"].toLocaleString();
+    document.getElementById("death").textContent = data["Total Deaths_text"].toLocaleString();
+    document.getElementById("recovered").textContent = data["Total Recovered_text"].toLocaleString();
 }
 
-fetch('https://disease.sh/v3/covid-19/all')
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
+const fetchCovidData = () =>{
+
+
+    fetch('https://covid-19.dataflowkit.com/v1/world')
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+        console.log(data["Active Cases_text"]);
     renderHtml(data);
     
-  });
+});
 
 var button = document.querySelector("button");
 button.addEventListener('click', () => {
-  var country = document.getElementById('searchButton').value;
-  console.log(country);
-  fetch(`https://corona.lmao.ninja/v2/countries/${country}?yesterday=true&strict=true&query`)
+    var country = document.getElementById('searchButton').value;
+    //   console.log(country);
+    fetch(`https://covid-19.dataflowkit.com/v1/${country}`)
     .then((response) => {
-      return response.json();
+        return response.json();
     })
     .then((data) => {
-      console.log(data);
-      if (!data.country) {
-          
-        document.getElementById("places").innerHTML = data.message;
-    } else {
-        document.getElementById("places").textContent = data.country;
-        renderHtml(data);
+        console.log(data);
+        if (!data.Country_text) {
+            // document.getElementById("places").innerHTML = data.message;
+            console.log("No country FOund");
+        } else {
+            document.getElementById("places").textContent = data["Country_text"];
+            renderHtml(data);
+            
+        }
         
-      }
-
     });
 });
+};
+
+fetchCovidData();
